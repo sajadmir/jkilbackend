07c1eb8544beaf464077b9f332e354a1279db87a
sajad <sajadmir261@gmail.com>
2023-01-18 23:33:43 +0530

    change2

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 
 const mongoose= require('mongoose');
-const mongoURI= process.env.DATABASE 
+const mongoURI= process.env.DATABASE ||  "mongodb://127.0.0.1:27017/jkil?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
 const connectToMongo=()=>{
 mongoose.connect(mongoURI,()=>{
     console.log("Connected to mongo DB Successfully");
