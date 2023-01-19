ed594dcbb200d80d29aa4c08eaab03ff8dfbae3a
sajad <sajadmir261@gmail.com>
2023-01-19 19:54:56 +0530

    change8

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 
 const mongoose= require('mongoose');
 //const mongoURI= process.env.DATABASE || "mongodb://127.0.0.1:27017/jkil?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
-const mongoURI= process.env.DATABASE || "mongodb://localhost:27017/jkil?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false"
+const mongoURI= process.env.DATABASE || "mongodb+srv://sajadmir38:pc141005@cluster0.0vbx3an.mongodb.net/jkil?retryWrites=true&w=majority"
 const connectToMongo=()=>{
 mongoose.connect(mongoURI,()=>{
     console.log("Connected to mongo DB Successfully");
-    console.log(process.env.DATABASE)
+    //console.log(process.env.DATABASE)
     
 })
 
